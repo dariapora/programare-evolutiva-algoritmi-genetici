bfa2fd725caa74adc9cec05874f1d6cc6facb714
dariapora <185256277+dariapora@users.noreply.github.com>
2026-05-12 13:19:03 +0300

    complete echipa de fotbal

--- echipefotbal.py
+++ echipefotbal.py
@@ -46,10 +46,53 @@ def generare_pop(n, dim):
         pop.append(individ)
     return pop, valori_jucatori
 
+def fps_pop(pop, qual):
+    dim = len(pop)
+    suma = np.sum(qual)
+    fps = []
+    for i in range(len(qual)):
+        fps.append(qual[i]/suma)
+    qfps = np.cumsum(fps)
+    return qfps
+
+def sigma_fps(pop, qual):
+    med = np.mean(qual)
+    var = np.std(qual)
+    newq = qual.copy()
+    for i in range(len(qual)):
+        newq[i] = max(0, (qual[i]-(med-2*var)))
+    if (np.sum(newq)==0):
+        qfps = fps_pop(pop, qual)
+    else:
+        qfps = fps_pop(pop, newq)
+    return qfps
+
+def selectie_ruleta(pop, qfps):
+    spop = pop.copy()
+    squal = qual.copy()
+    dim = len(pop)
+    for i in range(dim):
+        r = np.random.uniform(0, 1)
+        poz = np.where(qfps >= r)
+        isel = poz[0][0]
+        spop[i][:] = pop[isel][:]
+        squal[i] = qual[isel]
+    return spop, squal
+
 if __name__=="__main__":
     pop, valori_jucatori = generare_pop(10, 40)
     print("Populatie inainte de mutatie:")
     print(pop)
-    print("Populatie dupa mutatie:")
-    popm = mutatie_pop(pop, 0.9, valori_jucatori)
-    print(popm)
+    #print("Populatie dupa mutatie:")
+    #popm = mutatie_pop(pop, 0.9, valori_jucatori)
+    #print(popm)
+    #print("Vector de calitati:")
+    qual = []
+    for i in range(len(pop)):
+        qual.append(pop[i][-1])
+    #print(qual)
+    print("Populatie dupa selectie:")
+    pops, squal = selectie_ruleta(pop, sigma_fps(pop, qual))
+    print(pops)
+    print("Populatie selectata dupa mutatie, pm=0.6:")
+    print(mutatie_pop(pops, 0.6, valori_jucatori))
